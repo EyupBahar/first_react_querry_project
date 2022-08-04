897fc6fa6b431f987607552ae88bf79bba1da4e4
Eyup BAHAR <75902322+C7204-OMER@users.noreply.github.com>
2022-08-04 15:49:41 +0300

    feat: react-query project

---
+++ src/Characters.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import Character from "./Character";
+
+export default function Characters() {
+  const [page, setPage] = useState(40);
+
+  const fetchCharacters = async ({ queryKey }) => {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${queryKey[1]}`
+    );
+    return response.json();
+  };
+
+  const { data, status, isPreviousData } = useQuery(
+    ["characters", page],
+    fetchCharacters,
+    {
+      keepPreviousData: true,
+    }
+  );
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "error") {
+    return <div>Error</div>;
+  }
+
+  return (
+    <div className="characters">
+      {data.results.map((character, key) => (
+        <Character key={key} character={character} />
+      ))}
+      <div>
+        <button
+          onClick={() => setPage((old) => Math.max(old - 1, 1))}
+          disabled={page === 1}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => setPage((old) => old + 1)}
+          disabled={!data.info.next}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
